d1110fe672da44afdbe15d588c7c43afc2b15c8d
biswas98 <biswasbiplab98@gmail.com>
2023-08-08 16:22:17 +0530

    packages inclusion & navigation updates

--- App.tsx
+++ App.tsx
@@ -9,8 +9,10 @@ import {
 	useColorScheme,
 	View,
 } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-
+const Stack = createNativeStackNavigator();
 
 function App() {
 	
