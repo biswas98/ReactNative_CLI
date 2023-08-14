4ecfe17cdb76b8abf877923dffbd7cba12b3fe62
biswas98 <biswasbiplab98@gmail.com>
2023-08-14 14:43:52 +0530

    added component WeatherAPI

--- App.tsx
+++ App.tsx
@@ -11,6 +11,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './components/Login/Login'
 import Dashboard from './components/Dashboard/Dashboard'
 import API from './components/API/API'
+import WeatherAPI from './components/API/WeatherAPI'
 
 const Stack = createNativeStackNavigator();
 
@@ -19,9 +20,10 @@ function App() {
 	return (
 		<NavigationContainer>
 			<Stack.Navigator>
-				<Stack.Screen name='Login' component={Login}/>
+				{/* <Stack.Screen name='Login' component={Login}/>
 				<Stack.Screen name='Dashboard' component={Dashboard}/>
-				<Stack.Screen name='API' component={API}/>
+				<Stack.Screen name='API' component={API}/> */}
+				<Stack.Screen name='WeatherAPI' component={WeatherAPI}/>
 			</Stack.Navigator>
 		</NavigationContainer>
 	)
---
+++ components/API/WeatherAPI.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function WeatherAPI() {
+  return (
+    <View>
+      <Text>WeatherAPI</Text>
+    </View>
+  )
+}
