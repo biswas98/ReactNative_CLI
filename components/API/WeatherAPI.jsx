c4267129702c3d3e6fe0c8b3dbc55f245472e408
biswas98 <biswasbiplab98@gmail.com>
2023-08-14 23:36:54 +0530

    experimenting

--- components/API/WeatherAPI.jsx
+++ components/API/WeatherAPI.jsx
@@ -1,5 +1,6 @@
 import { View, Text } from 'react-native'
 import React, { useEffect } from 'react'
+// import 'dotenv/config'
 
 export default function WeatherAPI() {
 
@@ -20,7 +21,7 @@ export default function WeatherAPI() {
 
     return (
         <View>
-            <Text></Text>
+            <Text>jdfklsjfr</Text>
         </View>
     )
 }
