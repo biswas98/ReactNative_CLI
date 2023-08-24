6385da46615ffb92b929693c7de5bc08ee14948b
= <biswasbiplab98@gmail.com>
2023-08-24 23:46:49 +0530

    code refactoring

--- components/API/style.js
+++ components/API/style.js
@@ -1,10 +1,15 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
+    apiContainer: {
+        borderWidth: 1,
+        
+    },
     inputBox: {
         borderWidth: 1, 
         width: 200,
-    }
+    },
+    
 })
 
 export default styles;
