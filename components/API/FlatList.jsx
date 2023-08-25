b686e3079597023aa5fb7f275e5c0f842778d20d
Biplab Biswas <biswasbiplab98@gmail.com>
2023-08-25 19:38:35 +0530

    data initialization

--- components/API/FlatList.jsx
+++ components/API/FlatList.jsx
@@ -3,7 +3,34 @@ import React from 'react'
 
 export default function FlatList() {
 	const data = {
-		
+		0:{
+			head: 'ArcGIS.com Web Site',
+			message: 'Service is operation normally'
+		},
+		1:{
+			head: 'ArcGIS.com REST API',
+			message: 'Service is operation normally'
+		},
+		2:{
+			head: 'Hosted Feature Services',
+			message: 'Service is operation normally'
+		},
+		3:{
+			head: 'Feature Publishing',
+			message: 'Service is operation normally'
+		},
+		4:{
+			head: 'Hosted Tile Services',
+			message: 'Service is operation normally'
+		},
+		5:{
+			head: 'Tile Publishing',
+			message: 'Service is operation normally'
+		},
+		6:{
+			head: 'Esri Basemaps',
+			message: 'Service is operation normally'
+		}
 	}
 
   return (
