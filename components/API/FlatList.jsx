6e69f95f981bf16c13a769b23d5ccb4808541d44
Biplab Biswas <biswasbiplab98@gmail.com>
2023-08-25 19:28:45 +0530

    flatlist implemention

---
+++ components/API/FlatList.jsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function FlatList() {
+	const data = {
+		
+	}
+
+  return (
+	<View>
+	  <FlatList>
+
+	  </FlatList>
+	</View>
+  )
+}
