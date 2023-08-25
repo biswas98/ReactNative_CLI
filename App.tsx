b3fa117c6c1dead6a8eefc10409a9f29fd5fe2a7
Biplab Biswas <biswasbiplab98@gmail.com>
2023-08-25 20:52:43 +0530

    renamed FlatList component to FlatListPractice

--- App.tsx
+++ App.tsx
@@ -1,32 +1,29 @@
 import React from 'react';
-import {
-	StyleSheet,
-	Text,
-	View,
-	Platform
-} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {StyleSheet, Text, View, Platform} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import Login from './components/Login/Login'
-import Dashboard from './components/Dashboard/Dashboard'
-import API from './components/API/API'
-import WeatherAPI from './components/API/WeatherAPI'
+import Login from './components/Login/Login';
+import Dashboard from './components/Dashboard/Dashboard';
+import API from './components/API/API';
+import WeatherAPI from './components/API/WeatherAPI';
+import FlatListPractice from './components/API/FlatListPractice';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
-	// console.log(Platform.OS);
-	return (
-		<NavigationContainer>
-			<Stack.Navigator>
-				{/* <Stack.Screen name='Login' component={Login}/>
+  // console.log(Platform.OS);
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* <Stack.Screen name='Login' component={Login}/>
 				<Stack.Screen name='Dashboard' component={Dashboard}/>
 				<Stack.Screen name='API' component={API}/> */}
-				<Stack.Screen name='WeatherAPI' component={WeatherAPI}/>
-			</Stack.Navigator>
-		</NavigationContainer>
-	)
+        {/* <Stack.Screen name='WeatherAPI' component={WeatherAPI}/> */}
+        <Stack.Screen name="FlatListPractice" component={FlatListPractice} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
