02f416ea35e8a7c2c976f3164025cf3b8a1d3f6a
Biplab Biswas <biswasbiplab98@gmail.com>
2023-09-01 23:09:50 +0530

    testing new APIs

--- components/API/FlatListPractice.jsx
+++ components/API/FlatListPractice.jsx
@@ -2,65 +2,6 @@ import {View, Text, FlatList, StyleSheet} from 'react-native';
 import React from 'react';
 
 export default function FlatListPractice() {
-  const response = [
-    {
-      key: 0,
-      head: 'ArcGIS.com Web Site',
-      events: [],
-    },
-    {
-      key: 1,
-      head: 'ArcGIS.com REST API',
-      events: [],
-    },
-    {
-      key: 2,
-      head: 'Hosted Feature Services',
-      events: [{type: 'Performance issue'}],
-    },
-    {
-      key: 3,
-      head: 'Feature Publishing',
-      events: [{type: 'Service disruption'}],
-    },
-    {
-      key: 4,
-      head: 'Hosted Tile Services',
-      events: [{type: 'Informational Message'}],
-    },
-    {
-      key: 5,
-      head: 'Tile Publishing',
-      events: [{type: 'Performance issue'}],
-    },
-    {
-      key: 6,
-      head: 'Esri Basemaps',
-      events: [{type: 'Service is operation normally'}],
-    },
-    {
-      key: 7,
-      head: 'Geocoding',
-      events: [{type: 'Service is operation normally'}],
-    },
-    {
-      key: 8,
-      head: 'Directions and Routing',
-      events: [{type: 'Service is operation normally'}],
-    },
-    {
-      key: 9,
-      head: 'GeoEnrichment and Demographics',
-      events: [{type: 'Service is operation normally'}],
-    },
-    {
-      key: 10,
-      head: 'Spatial Analysis',
-      events: [{type: 'Service is operation normally'}],
-    },
-  ];
-  // console.log(data);
-
   return (
     <View>
       <FlatList
