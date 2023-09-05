ce70229801fe106cc61cf9cd7120859c24eac795
Biplab Biswas <biswasbiplab98@gmail.com>
2023-09-05 22:50:45 +0530

    handling multiple states using only one state variable

--- components/API/FlatListPractice.jsx
+++ components/API/FlatListPractice.jsx
@@ -1,8 +1,9 @@
-import {View, Text, FlatList, StyleSheet} from 'react-native';
+import {View, Text, FlatList, StyleSheet, TextInput} from 'react-native';
 import React, {useEffect, useState} from 'react';
 
 export default function FlatListPractice() {
   const [response, setResponse] = useState();
+
   const apiResponse = async () => {
     const URL = 'https://jsonplaceholder.typicode.com/posts';
     let response = await fetch(URL);
@@ -21,6 +22,9 @@ export default function FlatListPractice() {
         data={response}
         renderItem={({item}) => (
           <View style={styles.dataContainer}>
+            <View>
+              <TextInput style={styles.textInputStyle} />
+            </View>
             <View style={styles.textContainer}>
               <Text>{item.title}</Text>
             </View>
@@ -36,10 +40,13 @@ export default function FlatListPractice() {
 
 const styles = StyleSheet.create({
   dataContainer: {
+    borderWidth: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    margin: 10,
   },
   textContainer: {
     // borderWidth: 1,
   },
+  textInputStyle: {},
 });
