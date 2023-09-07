cbf08bccaf489b471df1310309846bb3d657d846
Biplab Biswas <biswasbiplab98@gmail.com>
2023-09-07 13:48:20 +0530

    added new Test component for testing purpose

---
+++ components/Test/Test.jsx
@@ -0,0 +1,16 @@
+import {View, Text, TextInput} from 'react-native';
+import React from 'react';
+
+export default function Test() {
+  return (
+    <View style={{margin: 20}}>
+      <Text>Write something...</Text>
+      <TextInput
+        style={{borderWidth: 1}}
+        placeholder="write..."
+        placeholderTextColor={'grey'}
+        secureTextEntry={false}
+      />
+    </View>
+  );
+}
